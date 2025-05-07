a47efeefa84d1a1eb10afae8ba689c21a496e348
FirdavsP31 <firdavs.palvanov11@gmail.com>
2025-05-07 15:50:21 +0500

    category added

--- news/views.py
+++ news/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render,get_object_or_404
-from .models import News
+from .models import News,Category
 
 # Create your views here.
 def homepage(request):
     posts = News.objects.all()
-    return render(request,'index.html',{'posts':posts})
+    categories = Category.objects.all()
+    return render(request,'index.html',
+                  {'posts':posts,
+                   'categories':categories})
 
 def detail(request,id):
     news = get_object_or_404(News,id=id)
-    return render(request,'detail.html',{'abc':news})
+    categories = Category.objects.all()
+    return render(request,'detail.html',
+                  {'abc':news,
+                    'categories':categories})
+
+def category_func(request,id):
+    categories = Category.objects.all()
+    news = News.objects.filter(category_id=id)
+
+    return render(request, 'category.html',
+                  {'news':news,
+                   'categories':categories})
